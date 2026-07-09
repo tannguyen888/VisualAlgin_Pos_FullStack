c7a01a4232d0ef32bf744c9eb7822697eaee3821
tannguyen888 <triumphdash00@gmai.com>
2026-07-08 22:03:43 -0400

    filter search

---
+++ VisualAlign_frontend/VisualAlignFE/src/pages/store/components/VehicleFilterBar.jsx
@@ -0,0 +1,15 @@
+// TODO: Component UI bo loc xe (Make -> Model -> Year).
+// TODO: Yeu cau UX:
+// TODO: - Disable model neu chua chon make.
+// TODO: - Clear model khi make thay doi.
+// TODO: - Trigger onChange(filterState) cho component cha.
+// TODO: - Ho tro loading state khi fetch option.
+
+function VehicleFilterBar() {
+    // TODO: Khai bao state local cho make/model/year.
+    // TODO: Su dung useQuery de lay make/model options.
+    // TODO: Goi props.onChange moi khi filter thay doi.
+    return null;
+}
+
+export default VehicleFilterBar;
---
+++ VisualAlign_frontend/VisualAlignFE/src/services/vehicleService.js
@@ -0,0 +1,8 @@
+// TODO: Service xe danh rieng cho vehicle catalog + compatible products.
+// TODO: Khong goi truc tiep API ngoai tu frontend; frontend chi goi backend cua ban.
+
+// TODO: export async function fetchVehicleMakes() {}
+// TODO: export async function fetchVehicleModels({ make, year }) {}
+// TODO: export async function fetchCompatibleProducts({ storeId, branchId, make, model, year }) {}
+
+// TODO: Khi implement, tai su dung requestJson tu services/apiClient.js.
